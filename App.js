cd7510066d93f94333d52cf87e5c3103116031ca
James T <jtara@tuta.io>
2018-03-12 16:46:41 -0700

    chg: better styling and structure; chg: filters input to only accept valid digits add: conversion algo for the 4 common number bases

--- App.js
+++ App.js
@@ -1,53 +1,102 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, TextInput } from 'react-native';
 
-class TextInputView extends Component {
+class NumberInputView extends Component {
   constructor(props) {
-      super(props);
-      // this.state = {text: 'BASE ' + props.numberBase};
+    let labels = {
+      2: "Bin",
+      8: "Oct",
+      10: "Dec",
+      16: "Hex",
+    }
+    props.label = labels[props.numberBase];
+
+    super(props);
+    this.state = {text: ''};
+
+    // used in regex pattern that matches non-accepted characters
+    this.validDigits = "";
+    for (let i = 0; i < this.props.numberBase; i++) {
+      this.validDigits += i;
+    }
+  }
+
+  /**
+   * Take the value, this.props.value and remove unaccepted characters
+   * @param {string} input 
+   */
+  filterInput(input) {
+    // console.log(this.validDigits);
+    console.log('input: ' + input);
+    console.log('state.text: ' + this.state.text);
+    let pattern = new RegExp("[^" + this.validDigits + "]*");
+    let filteredInput = input.replace(pattern, "");
+    return filteredInput;
   }
 
   render() {
     return (
-      <View style={{
-        flex: 0.2,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'powderblue',
-        }}>
-        <Text style={
-          styles.inputLabel
-        }>Bin
-        </Text>
+      <View style={styles.inputView}>
+        <Text style={styles.inputLabel}>{this.props.label}</Text>
         <TextInput
           placeholder={"enter number here"}
-          style={{flex: 0.8, alignItems: 'center'}}
-          onChangeText={(text) => this.setState({text})}
-          // value={(this.state.text)}
+          style={styles.textInput}
+          onChangeText={(text) => this.setState(
+            state=(prevState, props) => {
+              return {text: this.filterInput(text)}
+            })
+          }
+          value={this.state.text}
         />
-    </View>
+      </View>
     );
   }
 }
 
+class NumberInputController {
+  static convert(value, fromBase, toBase) {  
+    if (fromBase == "10") {
+      return this._convertFromDecimal(value, toBase);
+    } else if (toBase == "10") {
+      return this._convertToDecimal(value, fromBase);
+    } else if (fromBase == "2" || toBase == "2") {
+      // e.g.: 2A -> 42 -> 101010
+      return this._convertFromDecimal(
+        this._convertToDecimal(value, fromBase),
+        toBase
+      ).toString();
+    }
+  }
+
+  static _convertFromDecimal(value, toBase) {
+    return (new Number(value)).toString(toBase);
+  }
+
+  static _convertToDecimal(value, fromBase) {
+    return parseInt(value, fromBase);
+  }
+}
+
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.inputController = null;
+  }
+
   render() {
     return (
-      <View
-        style={
-          styles.container
-        }>
-        <View style={{
-            flex: 0.2,
-            justifyContent: 'center',
-            // alignItems: 'center',
-            backgroundColor: 'steelblue'
-            }}>
-          <Text style={{alignItems: 'center'}}>Base Converter - No Ads</Text>
+      <View style={styles.container}>
+        <View style={styles.navBar}>
+          <Text style={styles.title}>Base Converter</Text>
         </View>
-        <TextInputView
+        <NumberInputView
+          label={"Bin"}
           numberBase={2}
+        />
+        <NumberInputView
+          label={"Hex"}
+          numberBase={16}
         />
       </View>
     );
@@ -58,16 +107,50 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    padding: 20,
+    padding: 0,
   },
 
+  navBar: {
+    flex: 0.15,
+    justifyContent: 'center',
+    backgroundColor: 'steelblue'
+  },
+
+  // title within navBar
+  title: {
+    fontSize: 24,
+    textAlign: 'center',
+  },
+
+  // view of each input section
+  inputView: {
+    flex: 0.1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'lightblue',
+    borderRadius: 10,
+    padding: 0,
+    marginTop: 10,
+  },
+
+  // view used for label
   inputLabel: {
     alignItems: 'center',
-    lineHeight: 100,
+    lineHeight: 200,
     flex: 0.3,
     textAlign: 'center',
-    padding: 20,
+    padding: 0,
+    margin: 0,
     backgroundColor: 'skyblue',
-    borderRadius: 4
-  }
+    borderRadius: 10
+  },
+
+  // text input within input view
+  textInput: {
+    flex: 0.7, 
+    alignItems: 'center',
+    fontSize: 16,
+    padding: 10,
+  },
 });
